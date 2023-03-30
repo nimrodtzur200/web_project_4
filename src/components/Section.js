7fb4018c1de4967fa96f8ac97afe8e0d21c1e241
nimrodtzur200 <98587778+nimrodtzur200@users.noreply.github.com>
2023-03-30 16:27:08 +0300

    Add files via upload

---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+export class Section {
+    constructor({ data, renderer }, containerSelector) {
+      this._renderedItems = data; 
+      this._renderer = renderer; 
+      this._container = document.querySelector(containerSelector); 
+    }
+  
+    renderer() {
+      this._renderedItems.forEach((item) => {
+        this._renderer(item);
+      });
+    }
+  
+    addItem(element) {
+      this._container.append(element);
+    }
+  
+    prependItem(element) {
+      this._container.prepend(element);
+    }
+  }
